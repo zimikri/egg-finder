27ad960c82fa3ba57019af9ce535c35d5627aa5e
Zimay Krisztián <zimay.krisztian@gmail.com>
2020-04-12 18:20:48 +0200

    add alert when fox in the field

--- Game.ts
+++ Game.ts
@@ -65,6 +65,10 @@ export default class Game {
                     }
                 }, 300);
 
+            } else if (this._actualLevelSatus.lost) {
+                setTimeout(() => { 
+                    alert(`Hűha, sajnos erre járt a róka és elűzte a nyuszit!\nÚjra kell játszanod ezt a szintet!`)
+                }, 300);
             }
 
             this.setButtons();
